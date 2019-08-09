2d2772009cd43f0a18f473fa194b5b1de3626f41
Yusuke Shirota <winterleaves2219.443@gmail.com>
2019-08-09 15:54:46 +0900

    controller finish

---
+++ app/controllers/manage_orders_controller.rb
@@ -0,0 +1,2 @@
+class ManageOrdersController < ApplicationController
+end
---
+++ test/controllers/manage_end_users_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ManageEndUsersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
