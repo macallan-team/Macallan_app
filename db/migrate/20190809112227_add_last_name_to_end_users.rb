fb2a6352f098c4a55b5924bbf1839378081c3370
saya-94 <chontese.sy15@gmail.com>
2019-08-10 13:51:41 +0900

    カラムエラー修正

--- db/migrate/20190809112227_add_last_name_to_end_users.rb
+++ db/migrate/20190809112227_add_last_name_to_end_users.rb
@@ -1,6 +1,6 @@
 class AddLastNameToEndUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :end_users, :last_name, :string, null: false
+    add_column :end_users, :last_name, :string, null: false, default: ""
     add_column :end_users, :first_name, :string, null: false, default: ""
     add_column :end_users, :last_kana, :string, null: false, default: ""
     add_column :end_users, :first_kana, :string, null: false, default: ""
