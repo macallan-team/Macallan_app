36040a12c64c75ee0f19914a90d770f69c7f13a0
kushinoyuya <kusshy15059855@gmail.com>
2019-08-11 16:27:46 +0900

    kussea編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,24 @@
 class ItemsController < ApplicationController
 	def show
+		@item = Item.find(params[:id])
+		@item.user_id = current_user.id
 	end
 
 	def index
+		#とりあえず試しにnewメソッド入れた
+		@items = Item.all
+		@item = Item.where(['content LIKE ?', "%#{search}%"])
 	end
 
 	def search
+		@items = Item.all
 	end
+
+	private
+	def item_params
+		params.require(:user).permit(:name, :price, :image)
+	end
+
+
+
 end
