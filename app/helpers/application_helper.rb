dd0db8edad9644b819c69c7b84da2765df11ea8e
Yusuke Shirota <winterleaves2219.443@gmail.com>
2019-08-14 14:04:49 +0900

    fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def sales_status(item)
+        if item.sales_status == 'on_sale'
+            return '販売中'
+        else
+            return '販売停止中'
+        end
+    end
+
+
 end
