610171b6f22b184833caf49e395d5b8a38bb6bae
Yusuke Shirota <winterleaves2219.443@gmail.com>
2019-08-23 19:26:36 +0900

    fix_item_show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,8 +6,9 @@ class ItemsController < ApplicationController
 		@discs = @item.discs
 		if end_user_signed_in?
 			if current_end_user.reviews.any?
-				@review = Review.new
+
 				@review = current_end_user.reviews.find_by(item_id: @item.id)
+				@review = Review.new if @review.nil?
 			else
 				@review = Review.new
 			end
