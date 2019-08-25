63572c158db56f07ac66e8f7e48fb67c85427114
Yusuke Shirota <winterleaves2219.443@gmail.com>
2019-08-25 15:47:28 +0900

    fix itemvalidates

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,6 @@ class CartItem < ApplicationRecord
 
   belongs_to :end_user
   belongs_to :item
-  validates :name, :numericality => { :greater_than => 0 }
+  validates :count, :numericality => { :greater_than => 0 }
 
 end
