07ca28222fb5011d67607ad481ae484184a800a1
Yusuke Shirota <winterleaves2219.443@gmail.com>
2019-08-25 15:45:20 +0900

    fix itemshow2

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,6 @@ class CartItem < ApplicationRecord
 
   belongs_to :end_user
   belongs_to :item
-  validates :numericality => { :greater_than => 0 }
+  validates :name, :numericality => { :greater_than => 0 }
 
 end
