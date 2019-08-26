bfb1e2b28b7f97ff6679b969175f2b7dfcc4fb5e
saya-94 <chontese.sy15@gmail.com>
2019-08-26 15:49:31 +0900

    beforeアクションを設定

--- app/controllers/manage_orders_controller.rb
+++ app/controllers/manage_orders_controller.rb
@@ -1,5 +1,7 @@
 class ManageOrdersController < ApplicationController
-	# before_action :authenticate_admin!
+
+	before_action :authenticate_admin!
+
 	def update
 		@order = Order.find(params[:id])
 		@order.update(item_params)
